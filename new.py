6b154abde847738a6d18a4cb7126166dfc2cd20c
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2022-02-05 12:01:26 +0600

    commit

--- new.py
+++ new.py
@@ -1,4 +1,8 @@
-fruits = ["apple", "big"]
-for fruit in fruits:
-    print(fruits)
-print(fruits)
+# fruits = ["apple", "big"]
+# for fruit in fruits:
+#     print(fruits)
+# print(fruits)
+
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
