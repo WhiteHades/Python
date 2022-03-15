ac45d4ffc4e7ab46ed847133a754e547a1426bc9
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2022-03-15 14:33:12 +0600

    wip of def function

--- new.py
+++ new.py
@@ -1,6 +1,10 @@
 # How many cans of paint one needs to buy for a given height and width
+# Instructions: 1 can of paint can cover 5 square meters
 
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
+test_h = float(input("Height of wall: "))
+test_w = float(input("Width of wall: "))
 coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+paint_cal(height=test_h, width=test_w, cover=coverage)
+
+
+def paint_cal():
