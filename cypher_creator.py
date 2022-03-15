f0408d51c8720993522995870ea7625be2de6d19
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2022-03-16 05:37:27 +0600

    cypher creator

---
+++ cypher_creator.py
@@ -0,0 +1,22 @@
+# Cypher
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input(
+    "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plaintext, shiftamount):
+    cypher_word = ""
+    for letter in plaintext:
+        position = alphabet.index(letter)
+        n_position = position + shiftamount
+        n_letter = alphabet[n_position]
+        cypher_word += n_letter
+    print(f"The encoded text is {cypher_word}")
+
+
+encrypt(plaintext=text, shiftamount=shift)
