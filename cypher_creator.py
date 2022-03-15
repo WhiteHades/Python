c2d9d3f5632028c2ee6c51e7161f5bfb1c64003b
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2022-03-16 05:58:05 +0600

    added decoder functionality

--- cypher_creator.py
+++ cypher_creator.py
@@ -16,7 +16,20 @@ def encrypt(plaintext, shiftamount):
         n_position = position + shiftamount
         n_letter = alphabet[n_position]
         cypher_word += n_letter
-    print(f"The encoded text is {cypher_word}")
+    print(f"The encoded text is {cypher_word}.")
 
 
-encrypt(plaintext=text, shiftamount=shift)
+def decrypt(plaintext2, shiftamount2):
+    cypher_word2 = ""
+    for letter in plaintext2:
+        position = alphabet.index(letter)
+        n_position = position - shiftamount2
+        n_letter = alphabet[n_position]
+        cypher_word2 += n_letter
+    print(f"The decoded text is {cypher_word2}.")
+
+
+if direction == "encode":
+    encrypt(plaintext=text, shiftamount=shift)
+elif direction == "decode":
+    decrypt(plaintext2=text, shiftamount2=shift)
