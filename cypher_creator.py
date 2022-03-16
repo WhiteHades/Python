f6b35b5bff0470a7824488caada85b33f2d8d527
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2022-03-16 06:30:44 +0600

    finished

--- cypher_creator.py
+++ cypher_creator.py
@@ -1,35 +1,37 @@
 # Cypher
 
+from unittest import result
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
             'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input(
-    "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
+def cypher(start_text, shiftamount, cypher_direction):
+    end_text = ""
+    if cypher_direction == "decode":
+        shiftamount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shiftamount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"The {cypher_direction}d text is {end_text}.\n")
 
 
-def encrypt(plaintext, shiftamount):
-    cypher_word = ""
-    for letter in plaintext:
-        position = alphabet.index(letter)
-        n_position = position + shiftamount
-        n_letter = alphabet[n_position]
-        cypher_word += n_letter
-    print(f"The encoded text is {cypher_word}.")
-
-
-def decrypt(plaintext2, shiftamount2):
-    cypher_word2 = ""
-    for letter in plaintext2:
-        position = alphabet.index(letter)
-        n_position = position - shiftamount2
-        n_letter = alphabet[n_position]
-        cypher_word2 += n_letter
-    print(f"The decoded text is {cypher_word2}.")
-
-
-if direction == "encode":
-    encrypt(plaintext=text, shiftamount=shift)
-elif direction == "decode":
-    decrypt(plaintext2=text, shiftamount2=shift)
+should_continue = True
+while should_continue:
+    direction = input(
+        "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    cypher(start_text=text, shiftamount=shift, cypher_direction=direction)
+    result = input(
+        "Type 'yes' if you want to go again. Otherwise, type 'no'.\n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye.")
