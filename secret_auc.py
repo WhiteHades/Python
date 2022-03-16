e4a74ab9d7ad57fdcb52bdd086f0fe59ef46d52b
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2022-03-16 20:08:04 +0600

    fin

--- secret_auc.py
+++ secret_auc.py
@@ -22,7 +22,7 @@ logo = '''
 '''
 print(logo)
 bids = {}
-bidding_finished = False
+bidding = True
 
 
 def find_highest_bidder(bidding_record):
@@ -36,14 +36,14 @@ def find_highest_bidder(bidding_record):
     print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 
-while not bidding_finished:
+while bidding:
     name = input("What is your name?: ")
     price = int(input("What is your bid?: $"))
     bids[name] = price
     should_continue = input(
         "Are there any other bidders? Type 'yes or 'no'.\n")
     if should_continue == "no":
-        bidding_finished = True
+        bidding = False
         find_highest_bidder(bids)
     elif should_continue == "yes":
         cls()
