23a2cb3dc11a2b96ac22426baff07797019b4eb2
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2022-04-26 21:44:33 +0600

    .

--- new.py
+++ new.py
@@ -16,3 +16,27 @@ logo = """
 """
 
 print(logo)
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+OPERATIONS = {
+    '+': add,
+    '-': subtract,
+    '/': divide,
+    '*': multiply
+}
